50dd7175354aec6d640ac8242382caedf4a400eb
Jameson Aranda <jamesonaranda@gmail.com>
2017-02-14 14:03:59 -0700

    adding lab 5

---
+++ cs-1410/CS1410_Lab_04_jaranda/CS1410_Lab_04_jaranda/main.cpp
@@ -0,0 +1,32 @@
+// Author: Jameson Aranda
+// Assignment: CS1410_Lab_04_jaranda
+// Instructor: Craig Sharp
+// Class: CS 1410 -- 002
+// Date Written: 2/2/2017 2:43:41 PM
+// Description: 
+
+// I declare that the following code was written by me or provided
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive 
+// a zero on this project if I am found in violation of this policy.
+
+// Function Name: 
+// Purpose: 
+// Parameters: 
+// Returns: 
+// Pre-conditions: 
+// Post-conditions: 
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
+
